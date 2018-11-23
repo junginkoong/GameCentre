a260cea10cd2756a21b84485c7ee5789aa0d05af
jonathankoong <jung.koong@mail.utoronto.ca>
2018-11-23 13:03:36 -0500

    update triangle

--- app/src/main/java/com/example/prathamdesai13/gamecentre/Triangle.java
+++ app/src/main/java/com/example/prathamdesai13/gamecentre/Triangle.java
@@ -61,9 +61,9 @@ public class Triangle {
         vertexBuffer.position(0);
 
         // prepare shaders and OpenGL program
-        int vertexShader = ItemRenderer.loadShader(
+        int vertexShader = ZigZagRenderer.loadShader(
                 GLES20.GL_VERTEX_SHADER, vertexShaderCode);
-        int fragmentShader = ItemRenderer.loadShader(
+        int fragmentShader = ZigZagRenderer.loadShader(
                 GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode);
 
         mProgram = GLES20.glCreateProgram();             // create empty OpenGL Program
@@ -103,11 +103,11 @@ public class Triangle {
 
         // get handle to shape's transformation matrix
         mMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
-        ItemRenderer.checkGlError("glGetUniformLocation");
+        ZigZagRenderer.checkGlError("glGetUniformLocation");
 
         // Apply the projection and view transformation
         GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mvpMatrix, 0);
-        ItemRenderer.checkGlError("glUniformMatrix4fv");
+        ZigZagRenderer.checkGlError("glUniformMatrix4fv");
 
         // Draw the triangle
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
